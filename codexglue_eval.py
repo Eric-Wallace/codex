83b45932c232edd4fd9388d5b83b55ce1d35aec9
Jessy Lin <jessy81697@gmail.com>
2022-03-31 18:20:00 +0000

    Update script to generate codexglue ref

--- codexglue_eval.py
+++ codexglue_eval.py
@@ -1,17 +1,31 @@
 from datasets import load_dataset
 import sys
+from mosestokenizer import MosesDetokenizer
 
-def make_codexglue_reference_file(output_file: str, split="test"):
+def make_codexglue_reference_file(output_file: str, split="test", eval_type="cleaned"):
+    assert eval_type in ["full", "cleaned"]
     ds = load_dataset("code_x_glue_ct_code_to_text", "python", split=split)
+
+    if eval_type == "cleaned":
+        f_detok = open(output_file + ".detok", "w")
+        detokenize = MosesDetokenizer("en")
+
     with open(output_file, "w") as f:
-        print("new")
         for i in range(len(ds)):
             # Eval with true docstrs
-            gold = ds[i]["docstring"].encode("unicode_escape").decode("utf-8")
+            if eval_type == "full":
+                gold = ds[i]["docstring"].encode("unicode_escape").decode("utf-8")
+                f.write(f"{i}\t{gold}\n")
+
             # Eval with clean tokenized docstrings (used in paper)
-            #gold=' '.join(ds[i]['docstring_tokens']).replace('\n','')
-            #gold=' '.join(gold.strip().split())
-            f.write(f"{i}\t{gold}\n")
+            elif eval_type == "cleaned":
+                #  these postprocessing steps are the ones the authors use,
+                gold=' '.join(ds[i]['docstring_tokens']).replace('\n','')
+                gold=' '.join(gold.strip().split())
+                # apply our own detokenizer
+                gold_detok = detokenize(gold.split())
+                f.write(f"{i}\t{gold}\n")
+                f_detok.write(f"{i}\t{gold_detok}\n")
 
 def postprocess_model_outputs(path_to_model_outputs_txt: str):
     with open(path_to_model_outputs_txt) as f, \
